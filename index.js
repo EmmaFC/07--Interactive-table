127da48df3a0dc9c8bee7a93478cd1ed4adfa318
unknown <johndoe@example.com>
2021-12-30 02:59:29 +0100

    adding js functionality

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-for (let i=0; i<20; i++){
-let colorWord = document.getElementsByClassName('color_word')[i].innerText;
-let arrayColorWord = [];
-arrayColorWord.push(colorWord)
-console.log(arrayColorWord)
-console.log(colorWord)
+let wordList = []
+let i=0
+let colorWord
+
+let btnShowWords = document.getElementById('btn_show_words')
+let wordsContainer = document.getElementById('container_color_words')
+wordsContainer.style.display = 'none'
+
+btnShowWords.addEventListener('click', () =>{
+
+    if( wordsContainer.style.display != 'none'){
+        wordsContainer.style.display = 'none'
+        btnShowWords.innerHTML = 'Show'
+        console.log(':(')
+        return;
+    }
+        wordsContainer.style.display = 'inherit'
+        btnShowWords.innerHTML = 'Hide'
+        console.log(':)')
+})
+
+let add = (i,colorWord) => {  
+    wordList.push(colorWord)
 }
+let b = (colorWord) => {
+for (i=0; i<=wordList.length; i++){
+    colorWord = document.getElementsByClassName('color_word')[i].innerText;   
+    add(i,colorWord)    
+} }
+
+console.log(wordList)
+b(colorWord)
 
 
+/* function hideEachItem (wordList, colorWord, i) {
+
+    for (i=0; i<wordList.length; i++){
+        console.log(wordList)
+        setTimeout(() => {wordlist[i].style.display = 'inherit'}, 5000); 
+    
+    }
+} 
+ */
