840510903e7f6292c1a864dc3074ab41c091f815
Emma <emmafecor@hotmail.com>
2022-01-28 00:33:40 +0100

    interactive table changes

--- index.js
+++ index.js
@@ -1,117 +1,114 @@
-const cells = document.getElementById ("teal_cell_1");
-const cells2 = document.getElementById ("navy_cell_1");
-const cells3 = document.getElementById ("white_cell_1");
-const cells4 = document.getElementById ("purple_cell_1");
-const cells5 = document.getElementById ("olive_cell_1");
-const cells6 = document.getElementById ("black_cell_2");
-const cells7 = document.getElementById ("gray_cell_2");
-const titlecell = document.getElementById ("title_cell");
-const lastcell = document.getElementById ("last_cell");
+let cells = document.getElementById ("teal_cell_1");
+let cells2 = document.getElementById ("navy_cell_1");
+let cells3 = document.getElementById ("white_cell_1");
+let cells4 = document.getElementById ("purple_cell_1");
+let cells5 = document.getElementById ("olive_cell_1");
+let cells6 = document.getElementById ("black_cell_2");
+let cells7 = document.getElementById ("gray_cell_2");
+let titlecell = document.getElementById ("title_cell");
+let lastcell = document.getElementById ("last_cell");
 
 
 
 
-titlecell.onclick = function (){
+titlecell.addEventListener ("click", ()=>{
     titlecell.style.backgroundColor = "transparent";
     titlecell.innerText = "❤";
- 
-};
-lastcell.onclick = function (){
+})
+   
+lastcell.addEventListener ("click", ()=>{
     lastcell.style.backgroundColor = "transparent";
     lastcell.innerText = "❆❆❆❆❆";
-};
+})
 
 
 
-
-
-cells.onclick = function (){
+cells.addEventListener ("click", ()=>{
     cells.style.backgroundColor = "transparent";
-};
-
-cells2.onclick = function (){
+})
+cells2.addEventListener ("click", ()=>{
     cells2.style.backgroundColor = "transparent";
-};
+})
 
-cells3.onclick = function (){
+cells3.addEventListener ("click", ()=>{
     cells3.style.backgroundColor = "transparent";
-};
+})
 
-cells4.onclick = function (){
+cells4.addEventListener ("click", ()=>{
     cells4.style.backgroundColor = "transparent";
-};
+})
 
-cells5.onclick = function (){
+cells5.addEventListener ("click", ()=>{
     cells5.style.backgroundColor = "transparent";
-};
+})
 
-cells6.onclick = function (){
+cells6.addEventListener ("click", ()=>{
     cells6.style.backgroundColor = "transparent";
-};
+})
 
-cells7.onclick = function (){
+cells7.addEventListener ("click", ()=>{
    cells7.style.backgroundColor = "transparent";
-};
+})
 
 
 
 
-const changef1 = document.getElementById("blue_cell_1");
-const changef2 = document.getElementById("green_cell_1");
-const changef3 = document.getElementById("red_cell_1");
-const changef4 = document.getElementById("yellow_cell_1");
-const changef5 = document.getElementById("black_cell_1");
-const changef6 = document.getElementById("gray_cell_1");
-const changef7 = document.getElementById("olive_cell_2");
-const changef8 = document.getElementById("fuchsia_cell_1");
+let changef1 = document.getElementById("blue_cell_1");
+let changef2 = document.getElementById("green_cell_1");
+let changef3 = document.getElementById("red_cell_1");
+let changef4 = document.getElementById("yellow_cell_1");
+let changef5 = document.getElementById("black_cell_1");
+let changef6 = document.getElementById("gray_cell_1");
+let changef7 = document.getElementById("olive_cell_2");
+let changef8 = document.getElementById("fuchsia_cell_1");
 
 
 
 
-const f1 = document.createElement('img'); 
-f1.src = './img/f1.jpg'; 
+let f1 = document.createElement('img'); 
+f1.src = '/img/f1.jpg'; 
 f1.style.height = '0px';
 f1.style.margin = '0px';
 document.getElementById('blue_cell_1').appendChild(f1);
 
-const f2 = document.createElement('img'); 
-f2.src = './img/f2.jpg'; 
+let f2 = document.createElement('img'); 
+f2.src = '/img/f2.jpg'; 
 f2.style.height = '0px';
 f2.style.margin = '0px';
 document.getElementById('green_cell_1').appendChild(f2);
 
-const f3 = document.createElement("img"); 
-f3.src = "./img/f3.jpg"; 
+let f3 = document.createElement("img"); 
+f3.src = "/img/f3.jpg"; 
 f3.style.height = '0px';
 f3.style.margin = '0px';
 document.getElementById("red_cell_1").appendChild(f3);
 
-const f4 = document.createElement('img'); 
-f4.src = './img/f4.jpg'; 
+let f4 = document.createElement('img'); 
+f4.src = '/img/f4.jpg'; 
 f4.style.height = '0px';
 f4.style.margin = '0px';
 document.getElementById('yellow_cell_1').appendChild(f4);
 
-const f5 = document.createElement('img'); 
-f5.src = './img/f5.jpg'; 
+let f5 = document.createElement('img'); 
+f5.src = '/img/f5.jpg'; 
 f5.style.height = '0px';
 f5.style.margin = '0px';
 document.getElementById('black_cell_1').appendChild(f5);
 
-const f6 = document.createElement('img'); 
-f6.src = './img/f6.jpg'; 
+let f6 = document.createElement('img'); 
+f6.src = '/img/f6.jpg'; 
 f6.style.height = '0px';
 f6.style.margin = '0px';
 document.getElementById('gray_cell_1').appendChild(f6);
 
-const f7 = document.createElement('img'); 
-f7.src = './img/f7.jpg'; 
+let f7 = document.createElement('img'); 
+f7.src = '/img/f7.jpg'; 
 f7.style.height = '0px';
 f7.style.margin = '0px';
 document.getElementById('olive_cell_2').appendChild(f7);
 
-const f8 = document.createElement('img'); 
-f8.src = './img/f8.jpg'; 
+let f8 = document.createElement('img'); 
+f8.src = '/img/f8.jpg'; 
 f8.style.height = '0px';
 f8.style.margin = '0px';
 document.getElementById('fuchsia_cell_1').appendChild(f8);
@@ -119,7 +116,7 @@ document.getElementById('fuchsia_cell_1').appendChild(f8);
 
 
 
-changef1.onclick = function (){
+changef1.addEventListener ("click", ()=>{
     if(f1.style.height = "0px"){
         changef1.style.backgroundColor = "transparent";
         changef1.style.fontSize = "0px";
@@ -131,9 +128,9 @@ changef1.onclick = function (){
         f1.style.height = "0px";
         f1.style.width = "0px";
     }
-}
+})
 
-changef2.onclick = function (){
+changef2.addEventListener ("click", ()=>{
     if(f2.style.height = "0px"){
         changef2.style.backgroundColor = "transparent";
         changef2.style.fontSize = "0px";
@@ -144,9 +141,9 @@ changef2.onclick = function (){
         changef2.style.backgroundColor = "transparent";
         f2.style.height = "0px";
     }
-}
+})
 
-changef3.onclick = function (){
+changef3.addEventListener ("click", ()=>{
     if(f3.style.height = "0px"){
         changef3.style.backgroundColor = "transparent";
         changef3.style.fontSize = "0px";
@@ -157,9 +154,9 @@ changef3.onclick = function (){
         changef3.style.backgroundColor = "transparent";
         f3.style.height = "0px";
     }
-}
+})
 
-changef4.onclick = function (){
+changef4.addEventListener ("click", ()=>{
     if(f4.style.height = "0px"){
         changef4.style.backgroundColor = "transparent";
         changef4.style.fontSize = "0px";
@@ -170,9 +167,9 @@ changef4.onclick = function (){
         changef4.style.backgroundColor = "transparent";
         f4.style.height = "0px";
     }
-}
+})
 
-changef5.onclick = function (){
+changef5.addEventListener ("click", ()=>{
     if(f5.style.height = "0px"){
         changef5.style.backgroundColor = "transparent";
         changef5.style.fontSize = "0px";
@@ -183,9 +180,9 @@ changef5.onclick = function (){
         changef5.style.backgroundColor = "transparent";
         f5.style.height = "0px";
     }
-}
+})
 
-changef6.onclick = function (){
+changef6.addEventListener ("click", ()=>{
     if(f6.style.height = "0px"){
         changef6.style.backgroundColor = "transparent";
         changef6.style.fontSize = "0px";
@@ -196,9 +193,9 @@ changef6.onclick = function (){
         changef6.style.backgroundColor = "transparent";
         f6.style.height = "0px";
     }
-}
+})
 
-changef7.onclick = function (){
+changef7.addEventListener ("click", ()=>{
     if(f7.style.height = "0px"){
         changef7.style.backgroundColor = "transparent";
         changef7.style.fontSize = "0px";
@@ -209,9 +206,9 @@ changef7.onclick = function (){
         changef7.style.backgroundColor = "transparent";
         f7.style.height = "0px";
     }
-}
+})
 
-changef8.onclick = function (){
+changef8.addEventListener ("click", ()=>{
     if(f8.style.height = "0px"){
         changef8.style.backgroundColor = "transparent";
         changef8.style.fontSize = "0px";
@@ -222,17 +219,17 @@ changef8.onclick = function (){
         changef8.style.backgroundColor = "transparent";
         f8.style.height = "0px";
     }
-}
+})
 
 
 
 
-const content = document.getElementById("more_colors");
+let content = document.getElementById("more_colors");
 content.style.display = "none";
-const button = document.getElementById("Show_more");
+let button = document.getElementById("Show_more");
 
 
-button.onclick = function (){
+button.addEventListener ("click", ()=>{
     if(content.style.display === "none"){
         content.style.display = "block";
         button.innerHTML = "SHOW LESS";
@@ -241,7 +238,7 @@ button.onclick = function (){
         content.style.display = "none";
         button.innerHTML = "SHOW MORE";
     }
-};
+})
 
 
 
